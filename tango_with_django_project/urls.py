8141c461d8274d6377ff9179677d402106788702
Gaia <2266484a@student.gla.ac.uk>
2019-02-08 17:47:49 +0000

    Chapter 3

---
+++ tango_with_django_project/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf.urls import include
+from rango import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name = 'index'),
+    url(r'^rango/', include('rango.urls'))
+]
+
